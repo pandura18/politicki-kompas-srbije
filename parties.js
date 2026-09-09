766bca84f6aaaa22dc673a05382d3171265d7410
pandura18 <pandurapavle@gmail.com>
2026-09-09 16:22:49 +0200

    Update parties.js

--- parties.js
+++ parties.js
@@ -3,38 +3,38 @@ const parties2023 = [
 {
     name: "DS - Demokratska stranka - Srđan Milivojević",
     answers: [
-        2, 5, 4, 5, 4, 3, 5, 4, 4, 2, 5, 5, 2, 2, 5, 1, 2, 4, 2, 2, 5, 5, 1, 1, 1, 4, 1, 2, 3, 3
+        3, 5, 4, 5, 4, 3, 5, 4, 3, 2, 5, 5, 2, 2, 5, 1, 2, 3, 2, 2, 5, 5, 1, 1, 1, 3, 1, 2, 3, 3
     ],
     total: -5.33,
-    economic: -4,
-    social: -6,
+    economic: -3,
+    social: -6.5,
     foreign: -10,
-    global: -2,
+    global: -3,
     color: "#FFF500"
 },
 
 {
     name: "DJB - Dosta je bilo - Saša Radulović",
     answers: [
-        5, 4, 2, 2, 3, 2, 4, 5, 2, 4, 3, 4, 2, 3, 5, 1, 5, 5, 3, 2, 3, 1, 4, 4, 4, 5, 4, 5, 5, 4
+        5, 4, 2, 2, 1, 2, 4, 5, 1, 3, 3, 4, 2, 3, 5, 1, 5, 5, 1, 1, 3, 1, 4, 4, 3, 5, 5, 5, 5, 4
     ],
-    total: 2.5,
-    economic: 2.5,
-    social: -1.5,
-    foreign: 5,
-    global: 8,
+    total: 2.67,
+    economic: 4.5,
+    social: -3,
+    foreign: 4,
+    global: 9,
     color: "#F28300"
 },
 
 {
     name: "Dveri - Srpski pokret Dveri - Ivan Kostić",
     answers: [
-        3, 5, 5, 2, 5, 5, 3, 4, 5, 5, 1, 1, 5, 5, 3, 5, 5, 5, 5, 5, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        3, 5, 5, 4, 5, 5, 5, 3, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 3, 2, 1, 4, 5, 5, 5, 5, 5, 5, 5
     ],
-    total: 4.5,
-    economic: -5,
-    social: 9,
-    foreign: 9,
+    total: 2.83,
+    economic: -7.5,
+    social: 7,
+    foreign: 8,
     global: 10,
     color: "#7A4C11"
 },
@@ -42,38 +42,38 @@ const parties2023 = [
 {
     name: "EU - Ekološki ustanak - Aleksandar Jovanović Ćuta",
     answers: [
-        3, 4, 4, 5, 3, 3, 4, 3, 4, 1, 5, 4, 2, 3, 5, 2, 3, 4, 2, 1, 4, 3, 1, 2, 1, 5, 3, 2, 1, 2
+        2, 5, 3, 5, 4, 4, 5, 3, 3, 2, 5, 4, 2, 2, 5, 1, 3, 5, 1, 1, 4, 4, 1, 2, 1, 5, 1, 2, 2, 2
     ],
-    total: -3.5,
-    economic: -2,
-    social: -4.5,
-    foreign: -6,
-    global: -2,
+    total: -4.83,
+    economic: -4,
+    social: -5.5,
+    foreign: -7,
+    global: -3,
     color: "#3CE84B"
 },
 
 {
     name: "GPS - Solidarnost - Goran Ješić",
     answers: [
-        1, 3, 4, 4, 4, 5, 5, 5, 4, 2, 5, 5, 1, 2, 5, 1, 2, 3, 2, 2, 4, 5, 1, 1, 2, 4, 1, 1, 4, 3
+        1, 4, 3, 5, 4, 5, 5, 3, 3, 2, 5, 5, 1, 1, 5, 1, 1, 3, 1, 2, 4, 5, 1, 1, 1, 1, 1, 1, 2, 2
     ],
-    total: -5.17,
-    economic: -3.5,
-    social: -7,
-    foreign: -8,
-    global: -2,
+    total: -7.17,
+    economic: -4.5,
+    social: -8.5,
+    foreign: -9,
+    global: -8,
     color: "#E21B84"
 },
 
 {
     name: "JS - Jedinstvena Srbija - Dalibor Marković",
     answers: [
-        2, 4, 5, 3, 5, 4, 3, 4, 5, 4, 2, 1, 5, 5, 3, 1, 4, 4, 4, 4, 3, 4, 3, 4, 4, 5, 4, 5, 5, 5
+        2, 4, 5, 3, 5, 4, 3, 4, 4, 4, 2, 1, 5, 4, 2, 2, 4, 4, 1, 4, 3, 5, 3, 4, 4, 5, 5, 5, 5, 4
     ],
-    total: 1.67,
-    economic: -4.5,
-    social: 4.5,
-    foreign: 1,
+    total: 1.33,
+    economic: -4,
+    social: 3.5,
+    foreign: 0,
     global: 9,
     color: "#990003"
 },
@@ -81,13 +81,13 @@ const parties2023 = [
 {
     name: "LDP - Liberalno demokratska partija - Čedomir Jovanović",
     answers: [
-        3, 4, 2, 4, 2, 1, 3, 5, 2, 1, 5, 5, 1, 2, 5, 1, 3, 4, 1, 2, 5, 5, 1, 1, 1, 2, 1, 2, 3, 3
+        4, 3, 1, 4, 1, 1, 3, 5, 1, 1, 5, 5, 1, 1, 5, 1, 2, 2, 1, 2, 5, 5, 1, 1, 1, 1, 1, 1, 2, 2
     ],
-    total: -3.33,
-    economic: 3.5,
-    social: -6.5,
+    total: -3.76,
+    economic: 6,
+    social: -8.5,
     foreign: -10,
-    global: -4,
+    global: -8,
     color: "#7B1FA2"
 },
 
@@ -107,11 +107,11 @@ const parties2023 = [
 {
     name: "MI-SN - MI-Snaga naroda - Branimir Nestorović",
     answers: [
-        4, 4, 4, 3, 5, 5, 3, 4, 4, 5, 1, 1, 5, 5, 3, 2, 5, 5, 5, 5, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        3, 4, 4, 3, 5, 5, 3, 3, 4, 4, 1, 1, 5, 5, 4, 2, 5, 5, 5, 4, 2, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
-    total: 4.5,
-    economic: -3.5,
-    social: 7.5,
+    total: 4,
+    economic: -4,
+    social: 6.5,
     foreign: 9,
     global: 10,
     color: "#212121"
@@ -120,25 +120,25 @@ const parties2023 = [
 {
     name: "NLS - Novo lice Srbije - Miloš Parandilović",
     answers: [
-        4, 4, 4, 4, 4, 3, 3, 5, 3, 3, 3, 3, 5, 5, 5, 5, 4, 4, 4, 3, 3, 3, 1, 3, 3, 5, 3, 5, 4, 4
+       3, 4, 3, 3, 5, 3, 3, 5, 3, 3, 3, 3, 4, 5, 5, 5, 4, 4, 3, 3, 4, 3, 1, 3, 2, 5, 3, 5, 5, 4
     ],
-    total: 1.67,
+    total: 1.17,
     economic: -0.5,
-    social: 3.5,
-    foreign: -2,
-    global: 6,
+    social: 2.5,
+    foreign: -4,
+    global: 7,
     color: "#1976D2"
 },
 
 {
     name: "NOVA-D2SP - Nova stranka-Da se struka pita - Vladimir Kovačević",
     answers: [
-        5, 5, 2, 3, 5, 2, 2, 5, 4, 4, 5, 5, 1, 4, 5, 1, 4, 4, 1, 1, 5, 5, 1, 1, 1, 1, 4, 2, 2, 4
+        5, 5, 2, 3, 5, 2, 2, 5, 3, 4, 5, 5, 1, 3, 5, 1, 3, 4, 1, 1, 4, 5, 1, 1, 1, 2, 1, 2, 3, 5
     ],
-    total: -4,
-    economic: 0.5,
-    social: -5.5,
-    foreign: -10,
+    total: -3.67,
+    economic: 1,
+    social: -6.5,
+    foreign: -9,
     global: -2,
     color: "#FEFF79"
 },
@@ -146,12 +146,12 @@ const parties2023 = [
 {
     name: "NPS - Narodni pokret Srbije - Miroslav Aleksić",
     answers: [
-        3, 5, 4, 4, 4, 3, 4, 5, 3, 2, 2, 3, 4, 5, 5, 1, 3, 4, 3, 3, 4, 5, 2, 2, 2, 4, 2, 3, 4, 4
+        4, 5, 3, 3, 5, 3, 5, 5, 2, 3, 2, 3, 3, 3, 5, 1, 3, 5, 2, 2, 5, 5, 2, 1, 1, 5, 4, 4, 4, 4
     ],
     total: -1.33,
-    economic: -1.5,
-    social: 0.5,
-    foreign: -2,
+    economic: -1,
+    social: -1.5,
+    foreign: -9,
     global: 6,
     color: "#0288D1"
 },
@@ -159,25 +159,25 @@ const parties2023 = [
 {
     name: "NS - Narodna stranka - Vladimir Gajić",
     answers: [
-        4, 5, 3, 2, 5, 5, 4, 4, 4, 2, 2, 2, 4, 5, 5, 5, 4, 4, 4, 4, 4, 1, 4, 5, 4, 5, 5, 5, 5, 4
+        4, 5, 3, 2, 5, 5, 4, 4, 3, 3, 2, 2, 4, 5, 5, 4, 4, 5, 5, 3, 3, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
-    total: 3.17,
+    total: 3.83,
     economic: -2,
     social: 4.5,
-    foreign: 5,
-    global: 9,
+    foreign: 8,
+    global: 10,
     color: "#616161"
 },
 
 {
     name: "NDSS - Nova demokratska stranka Srbije - Miloš Jovanović",
     answers: [
-        5, 2, 2, 3, 4, 4, 2, 5, 3, 3, 2, 2, 5, 5, 3, 4, 5, 4, 5, 4, 1, 2, 5, 5, 5, 5, 5, 5, 5, 5
+        5, 3, 3, 3, 4, 5, 2, 5, 3, 4, 2, 2, 5, 5, 5, 5, 5, 5, 5, 3, 3, 1, 4, 5, 4, 5, 5, 5, 5, 5
     ],
-    total: 6.17,
-    economic: 2.5,
-    social: 6.5,
-    foreign: 9,
+    total: 4.83,
+    economic: 0.5,
+    social: 6,
+    foreign: 6,
     global: 10,
     color: "#3119FF"
 },
@@ -185,12 +185,12 @@ const parties2023 = [
 {
     name: "POKS - Pokret obnove kraljevine Srbije - Vojislav Mihailović",
     answers: [
-        4, 4, 5, 2, 5, 5, 2, 4, 5, 5, 1, 1, 5, 5, 3, 4, 5, 5, 5, 4, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        4, 4, 4, 2, 5, 5, 2, 4, 4, 3, 2, 1, 5, 5, 5, 5, 5, 4, 5, 3, 1, 1, 5, 5, 4, 5, 5, 5, 5, 5
     ],
-    total: 4.83,
-    economic: -3.5,
-    social: 8,
-    foreign: 10,
+    total: 4.67,
+    economic: -1.5,
+    social: 6,
+    foreign: 9,
     global: 10,
     color: "#A1887F"
 },
@@ -198,11 +198,11 @@ const parties2023 = [
 {
     name: "PS - Pokret socijalista - Aleksandar Vulin",
     answers: [
-        2, 5, 5, 3, 5, 5, 5, 3, 4, 4, 1, 1, 4, 5, 4, 1, 5, 5, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        2, 5, 4, 3, 5, 5, 5, 3, 4, 4, 2, 2, 5, 5, 2, 1, 5, 5, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
-    total: 2.5,
-    economic: -6.5,
-    social: 6,
+    total: 3.5,
+    economic: -6,
+    social: 6.5,
     foreign: 10,
     global: 10,
     color: "#7D0002"
@@ -211,39 +211,39 @@ const parties2023 = [
 {
     name: "PSG - Pokret slobodnih građana - Pavle Grbović",
     answers: [
-        3, 5, 3, 4, 3, 2, 4, 5, 4, 1, 5, 5, 1, 2, 5, 1, 1, 4, 1, 1, 4, 5, 1, 1, 1, 2, 1, 1, 2, 2
+        3, 4, 3, 4, 3, 2, 4, 5, 2, 2, 5, 5, 1, 2, 5, 1, 1, 4, 1, 1, 5, 5, 1, 1, 1, 1, 1, 1, 2, 2
     ],
     total: -5.33,
-    economic: 0,
+    economic: 1,
     social: -8,
-    foreign: -9,
-    global: -7,
+    foreign: -10,
+    global: -8,
     color: "#0097A7"
 },
 
 {
     name: "PSS-BK - Pokret snaga Srbije - Bogoljub Karić",
     answers: [
-        5, 2, 3, 2, 5, 4, 2, 5, 2, 3, 1, 1, 4, 4, 3, 5, 2, 3, 3, 5, 4, 4, 2, 2, 2, 5, 4, 3, 4, 3
+        5, 3, 4, 2, 5, 3, 2, 4, 2, 3, 1, 1, 4, 4, 4, 5, 3, 3, 3, 3, 4, 4, 3, 2, 3, 4, 4, 4, 5, 4
     ],
-    total: 2.17,
-    economic: 2.5,
-    social: 4.5,
-    foreign: -5,
-    global: 4,
+    total: 2,
+    economic: 1.5,
+    social: 3.5,
+    foreign: -3,
+    global: 6,
     color: "#F9A825"
 },
 
 {
     name: "PUPS - Partija ujedenjenih penzionera Srbije - Milan Krkobabić",
     answers: [
-        2, 4, 5, 4, 5, 4, 4, 4, 5, 3, 3, 3, 4, 5, 3, 1, 4, 3, 4, 4, 3, 3, 3, 3, 3, 5, 3, 4, 4, 4
+        2, 4, 5, 3, 5, 4, 4, 3, 5, 3, 3, 3, 4, 5, 2, 1, 4, 3, 4, 4, 3, 4, 3, 3, 3, 5, 3, 5, 4, 4
     ],
-    total: -0.17,
+    total: 0,
     economic: -5,
-    social: 2,
-    foreign: 0,
-    global: 5,
+    social: 2.5,
+    foreign: -1,
+    global: 6,
     color: "#FF6A6C"
 },
 
@@ -251,24 +251,24 @@ const parties2023 = [
 {
     name: "SDS - Socialdemokratska stranka - Boris Tadić",
     answers: [
-        2, 5, 4, 4, 4, 3, 5, 3, 4, 2, 5, 5, 2, 3, 5, 1, 2, 4, 2, 2, 4, 5, 1, 1, 1, 4, 1, 3, 3, 3
+        3, 5, 5, 4, 4, 4, 5, 5, 3, 2, 5, 5, 3, 3, 5, 1, 2, 4, 2, 2, 5, 5, 1, 1, 1, 4, 1, 2, 3, 3
     ],
-    total: -5.17,
-    economic: -4,
-    social: -5.5,
-    foreign: -9,
-    global: -1,
+    total: -4.67,
+    economic: -3,
+    social: -5,
+    foreign: -10,
+    global: -2,
     color: "#FFC7C7"
 },
 
 {
     name: "SNP - Srpska narodna partija - Nenad Popović",
     answers: [
-        3, 4, 4, 3, 4, 4, 4, 4, 3, 4, 2, 1, 5, 5, 3, 2, 5, 4, 5, 4, 2, 2, 4, 5, 5, 5, 5, 5, 5, 5
+        4, 3, 3, 3, 4, 3, 2, 5, 3, 3, 1, 1, 5, 5, 3, 2, 5, 5, 5, 3, 3, 1, 5, 5, 4, 5, 5, 5, 5, 5
     ],
     total: 3.67,
-    economic: -2.5,
-    social: 6,
+    economic: 1.5,
+    social: 6.5,
     foreign: 7,
     global: 10,
     color: "#B601CF"
@@ -277,12 +277,12 @@ const parties2023 = [
 {
     name: "SNS - Srpska napredna stranka - Miloš Vučević",
     answers: [
-        4, 4, 3, 3, 5, 4, 2, 5, 4, 1, 3, 3, 4, 5, 2, 3, 4, 3, 5, 5, 4, 4, 3, 4, 3, 5, 5, 3, 5, 4
+        3, 5, 4, 3, 5, 4, 2, 5, 4, 1, 4, 3, 5, 5, 2, 1, 4, 3, 5, 5, 4, 4, 3, 4, 4, 5, 5, 4, 4, 4
     ],
-    total: 2.83,
-    economic: 0.5,
-    social: 4.5,
-    foreign: -1,
+    total: 2,
+    economic: -1,
+    social: 3.5,
+    foreign: 0,
     global: 7,
     color: "#0057b8"
 },
@@ -290,37 +290,37 @@ const parties2023 = [
 {
     name: "SPS - Socijalistička partija Srbija - Ivica Dačić",
     answers: [
-        2, 5, 5, 3, 5, 5, 4, 4, 5, 3, 3, 2, 4, 5, 2, 1, 4, 3, 5, 4, 3, 3, 4, 4, 4, 5, 5, 4, 5, 5
+        2, 5, 5, 4, 5, 5, 5, 4, 4, 2, 3, 3, 3, 5, 2, 1, 4, 4, 5, 4, 4, 4, 3, 4, 4, 5, 5, 5, 5, 5
     ],
-    total: 1.33,
+    total: 0.83,
     economic: -5.5,
-    social: 3.5,
-    foreign: 3,
-    global: 9,
+    social: 3,
+    foreign: 0,
+    global: 10,
     color: "#880E4F"
 },
 
 {
     name: "SRS - Srpska radikalna stranka - Vojislav Šešelj",
     answers: [
-        4, 4, 4, 2, 5, 5, 2, 4, 5, 5, 1, 1, 5, 5, 2, 1, 5, 4, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        5, 5, 4, 2, 5, 4, 3, 4, 2, 5, 1, 1, 5, 5, 4, 1, 5, 4, 5, 5, 1, 1, 5, 5, 5, 4, 5, 5, 5, 5
     ],
     total: 4.67,
-    economic: -3,
-    social: 7,
+    economic: -1.5,
+    social: 6,
     foreign: 10,
-    global: 10,
+    global: 9,
     color: "#011375"
 },
 
 {
     name: "SRCE - Srbija centar - Zdravko Ponoš",
     answers: [
-        3, 5, 3, 5, 4, 4, 4, 5, 4, 2, 5, 2, 2, 3, 5, 1, 2, 4, 2, 2, 4, 5, 1, 1, 1, 4, 1, 2, 3, 3
+        3, 5, 3, 5, 4, 4, 4, 5, 3, 2, 5, 3, 2, 3, 5, 1, 2, 4, 2, 2, 4, 5, 1, 1, 1, 3, 1, 2, 3, 4
     ],
     total: -4,
-    economic: -2.5,
-    social: -4,
+    economic: -2,
+    social: -4.5,
     foreign: -9,
     global: -2,
     color: "#15E3FF"
@@ -329,24 +329,24 @@ const parties2023 = [
 {
     name: "SSP - Stranka slobode i pravde - Dragan Đilas",
     answers: [
-        2, 5, 5, 4, 5, 5, 5, 4, 5, 2, 5, 5, 2, 2, 5, 1, 2, 4, 2, 2, 4, 5, 1, 1, 1, 5, 1, 2, 3, 3
+        2, 5, 5, 5, 5, 5, 5, 4, 5, 2, 5, 5, 1, 2, 5, 1, 2, 3, 1, 2, 5, 5, 1, 1, 1, 5, 1, 2, 2, 3
     ],
-    total: -5.67,
-    economic: -6,
-    social: -6,
-    foreign: -9,
-    global: -1,
+    total: -6.67,
+    economic: -6.5,
+    social: -7.5,
+    foreign: -10,
+    global: -2,
     color: "#F04547"
 },
 
 {
     name: "SSZ - Srpska stranka Zavetnici - Milica Đurđević Stamenkovski",
     answers: [
-        5, 3, 2, 3, 5, 5, 3, 5, 1, 4, 1, 1, 5, 5, 3, 2, 5, 4, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        5, 4, 2, 3, 5, 5, 3, 4, 2, 5, 1, 1, 5, 5, 4, 2, 5, 4, 5, 4, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
-    total: 6,
-    economic: 1,
-    social: 7,
+    total: 5,
+    economic: -1,
+    social: 6,
     foreign: 10,
     global: 10,
     color: "#C2185B"
@@ -355,39 +355,39 @@ const parties2023 = [
 {
     name: "ZS - Zeleni Srbije - Ivan Karić",
     answers: [
-        1, 5, 4, 5, 4, 5, 5, 5, 4, 2, 5, 5, 2, 3, 5, 2, 3, 4, 2, 2, 4, 4, 2, 2, 2, 3, 1, 2, 4, 3
+        1, 5, 4, 5, 4, 4, 5, 5, 3, 2, 5, 5, 1, 3, 5, 1, 3, 5, 2, 2, 4, 5, 1, 2, 2, 2, 1, 2, 4, 3
     ],
-    total: -4.33,
-    economic: -5,
-    social: -4.5,
-    foreign: -5,
-    global: -2,
+    total: -4.67,
+    economic: -4,
+    social: -5,
+    foreign: -7,
+    global: -3,
     color: "#7ACF40"
 },
 
 {
     name: "ZS - Zdrava Srbija - Milan Stamatović",
     answers: [
-        3, 4, 4, 4, 4, 4, 3, 5, 3, 4, 2, 2, 4, 5, 4, 2, 5, 4, 5, 4, 3, 1, 3, 4, 3, 5, 4, 5, 5, 5
+        3, 4, 4, 4, 5, 4, 3, 4, 3, 4, 2, 1, 5, 5, 3, 2, 5, 5, 4, 4, 3, 1, 3, 5, 4, 5, 5, 5, 5, 5
     ],
-    total: 2.83,
-    economic: -2,
-    social: 4.5,
-    foreign: 3,
-    global: 9,
+    total: 3.5,
+    economic: -3,
+    social: 6,
+    foreign: 5,
+    global: 10,
     color: "#CFA136"
 },
 
 {
     name: "ZLF - Zeleno levi front - Biljana Đorđević",
     answers: [
-        1, 5, 4, 4, 5, 4, 5, 2, 3, 1, 5, 5, 1, 2, 5, 1, 1, 5, 1, 1, 4, 5, 1, 1, 1, 4, 1, 1, 2, 2
+        1, 5, 4, 5, 4, 4, 5, 3, 3, 1, 5, 5, 1, 1, 5, 1, 1, 4, 1, 1, 4, 5, 1, 1, 1, 3, 1, 1, 2, 2
     ],
-	total: -6.5,
-economic: -5,
-social: -7.5,
+	total: -6.83,
+economic: -4.5,
+social: -8.5,
 foreign: -9,
-global: -5,
+global: -6,
 color:"#008C0C"
 },
 
@@ -398,57 +398,57 @@ const parties2023Coalitions = [
     {
     name: "Srbija ne sme da stane",
     answers: [
-        4, 4, 3, 3, 5, 4, 2, 5, 4, 1, 3, 3, 4, 5, 2, 3, 4, 3, 5, 5, 4, 4, 3, 4, 3, 5, 5, 3, 5, 4
+        3, 5, 4, 3, 5, 4, 2, 5, 4, 1, 4, 3, 5, 5, 2, 1, 4, 3, 5, 5, 4, 4, 3, 4, 4, 5, 5, 4, 4, 4
     ],
-	total: 2.67,
-economic: 0.5,
-social: 4.5,
-foreign: -1,
-global: 7,
+    total: 2,
+    economic: -1,
+    social: 3.5,
+    foreign: 0,
+    global: 10,
 color:"#242970"
 },
 
     {
     name: "Srbija protiv nasilja",
     answers: [
-        2, 5, 4, 4, 4, 4, 4, 4, 4, 2, 4, 4, 2, 3, 5, 1, 2, 4, 2, 2, 4, 5, 1, 1, 1, 4, 1, 2, 3, 3
+        3, 5, 4, 4, 5, 4, 5, 4, 3, 2, 4, 4, 2, 2, 5, 1, 2, 4, 2, 2, 5, 5, 1, 1, 1, 4, 2, 2, 3, 3
     ],
-	total: -4.83,
+	total: -4.67,
 economic: -3.5,
-social: -4.5,
-foreign: -9,
-global: -2,
+social: -5,
+foreign: -10,
+global: -1,
 color:"#F22C27"
 },
 
     {
-    name: "Ivica dačić - Premijer Srbije",
+    name: "Ivica dačić-Premijer Srbije",
     answers: [
-        2, 5, 5, 3, 5, 5, 4, 4, 5, 3, 3, 2, 4, 5, 2, 1, 4, 3, 5, 4, 3, 3, 4, 4, 4, 5, 5, 4, 5, 5
+        2, 5, 5, 4, 5, 5, 4, 4, 4, 3, 3, 3, 3, 5, 2, 1, 4, 4, 4, 4, 4, 4, 3, 4, 4, 5, 5, 5, 5, 5
     ],
-	total: 1.33,
+	total: 0.67,
 economic: -5.5,
-social: 3.5,
-foreign: 3,
-global: 9,
+social: 2.5,
+foreign: 0,
+global: 10,
 color:"#A60C19"
 },
 
     {
     name: "Nacionalo demokratska alternativa",
     answers: [
-        5, 3, 3, 3, 4, 4, 2, 5, 4, 4, 2, 2, 5, 5, 3, 4, 5, 4, 5, 4, 1, 2, 5, 5, 5, 5, 5, 5, 5, 5
+        5, 3, 3, 3, 4, 5, 2, 5, 3, 4, 2, 2, 5, 5, 5, 5, 5, 5, 5, 3, 2, 1, 4, 5, 4, 5, 5, 5, 5, 5
     ],
-	total: 5.5,
+	total: 5,
 economic: 0.5,
-social: 6.5,
-foreign: 9,
+social: 6,
+foreign: 7,
 global: 10,
 color:"#616264"
 },
 
     {
-    name: "Mi - Glas iz naroda",
+    name: "Mi-Glas iz naroda",
     answers: [
         3, 4, 4, 3, 5, 5, 4, 4, 4, 5, 2, 1, 5, 5, 2, 2, 5, 5, 5, 4, 3, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
@@ -463,11 +463,11 @@ color:"#47493B"
     {
     name: "Nacionalno okupljanje",
     answers: [
-        4, 4, 3, 3, 5, 5, 3, 5, 3, 4, 1, 1, 5, 5, 3, 3, 5, 4, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        4, 4, 3, 3, 5, 5, 4, 4, 3, 5, 1, 1, 5, 5, 4, 3, 5, 4, 5, 4, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
-	total: 5.33,
-economic: -1.5,
-social: 7.5,
+	total: 4.5,
+economic: -3,
+social: 6.5,
 foreign: 10,
 global: 10,
 color:"#0215B5"
@@ -476,24 +476,24 @@ color:"#0215B5"
     {
     name: "Srpska radikalna stranka",
     answers: [
-        4, 4, 4, 2, 5, 5, 2, 4, 5, 5, 1, 1, 5, 5, 2, 1, 5, 4, 5, 5, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5
+        5, 5, 4, 2, 5, 4, 3, 4, 2, 5, 1, 1, 5, 5, 4, 1, 5, 4, 5, 5, 1, 1, 5, 5, 5, 4, 5, 5, 5, 5
     ],
-	total: 4.33,
-economic: -3,
-social: 7,
-foreign: 10,
-global: 10,
+    total: 4.67,
+    economic: -1.5,
+    social: 6,
+    foreign: 10,
+    global: 9,
 color:"#0000FF"
 },
 
     {
     name: "Dobro jutro Srbijo",
     answers: [
-        3, 5, 3, 3, 4, 3, 5, 4, 3, 3, 4, 5, 2, 3, 5, 1, 3, 4, 2, 2, 4, 3, 2, 2, 2, 4, 2, 4, 4, 3
+        4, 5, 4, 3, 3, 3, 5, 5, 2, 2, 4, 5, 3, 3, 5, 1, 3, 4, 2, 2, 4, 3, 2, 2, 2, 4, 3, 3, 4, 3
     ],
-	total: -2.5,
-economic: -2,
-social: -4.5,
+	total: -1.67,
+economic: 0,
+social: -4,
 foreign: -4,
 global: 2,
 color:"#CBA656"
@@ -502,26 +502,26 @@ color:"#CBA656"
     {
     name: "Narodna stranka",
     answers: [
-        4, 5, 3, 2, 5, 5, 4, 4, 4, 2, 2, 2, 4, 5, 5, 5, 4, 4, 4, 4, 4, 1, 4, 5, 4, 5, 5, 5, 5, 4
+        4, 5, 3, 2, 5, 5, 4, 4, 3, 3, 2, 2, 4, 5, 5, 4, 4, 5, 5, 3, 3, 1, 5, 5, 5, 5, 5, 5, 5, 5
     ],
-	total: 3.17,
+	total: 3.83,
 economic: -2,
 social: 4.5,
-foreign: 5,
-global: 9,
+foreign: 8,
+global: 10,
 color:"#1E90FF"
 },
 
     {
     name: "Mora drugačije",
     answers: [
-        3, 4, 2, 4, 2, 1, 3, 5, 2, 1, 5, 5, 1, 2, 5, 1, 3, 4, 1, 2, 5, 5, 1, 1, 1, 2, 1, 2, 3, 3
+        4, 3, 1, 4, 1, 1, 3, 5, 1, 1, 5, 5, 1, 1, 5, 1, 2, 2, 1, 2, 5, 5, 1, 1, 1, 1, 1, 1, 2, 2
     ],
-	total: -3.33,
-economic: 3.5,
-social: -6.5,
+	total: -3.83,
+economic: 6,
+social: -8.5,
 foreign: -10,
-global: -4,
+global: -8,
 color:"#6A00D7"
 },
 
@@ -529,26 +529,13 @@ color:"#6A00D7"
     {
     name: "Srbija na zapadu",
     answers: [
-        5, 5, 2, 3, 5, 2, 2, 5, 4, 4, 5, 5, 1, 4, 5, 1, 4, 4, 1, 1, 5, 5, 1, 1, 1, 1, 4, 2, 2, 4
+        5, 5, 2, 3, 5, 2, 2, 5, 3, 4, 5, 5, 1, 3, 5, 1, 3, 4, 1, 1, 4, 5, 1, 1, 1, 2, 1, 2, 3, 5
     ],
 	total: -3.67,
-economic: 0.5,
-social: -5.5,
-foreign: -10,
+economic: 1,
+social: -6.5,
+foreign: -9,
 global: -2,
 color:"#03ABB5"
-},
-
-    {
-    name: "Prosečan birač na izborima 2023",
-    answers: [
-        3, 4, 3, 3, 5, 4, 3, 5, 4, 2, 3, 3, 4, 4, 3, 2, 4, 3, 4, 4, 4, 4, 3, 3, 3, 5, 4, 3, 4, 4
-    ],
-	total: 0.8,
-economic: -1,
-social: 2,
-foreign: -2,
-global: 5,
-color:"#BFFAFF"
 },
 ];
